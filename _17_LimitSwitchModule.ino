6f7929384ee3fa33cefba0e4c522ee587e31b0e2
Adeept <support@adeept.com>
2016-09-19 12:07:43 +0800

    17 Limit Switch Module
    Lesson 17 code

---
+++ _17_LimitSwitchModule.ino
@@ -0,0 +1,25 @@
+/***********************************************************
+File name: _17_LimitSwitchModule.ino
+Description: The information of limit switch module has been
+             detected by UNO R3,and displayed in the serial monitor 
+Website: www.adeept.com
+E-mail: support@adeept.com
+Author: Tom
+Date: 2016/06/15 
+***********************************************************/
+int limitswitchPin=8;            //Set the digital 8 to the S pin
+void setup() {
+  pinMode( limitswitchPin,INPUT);//initialize the S pin as input
+  Serial.begin(9600);           //opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  if(digitalRead(limitswitchPin)==LOW)
+  {
+     Serial.println("Limit switch is not triggering!"); //send data to the serial monitor
+  }else
+  {
+     Serial.println("Limit Switch triggered!");         //send data to the serial monitor
+  }
+  delay(500);                                            //delay 0.5s
+}
